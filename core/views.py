53149054c1c7ebca35f645b5e6da4c892ee62d1e
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2022-01-11 00:05:40 -0300

    form

--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib import messages
+
+from .forms import ContactForm
 
 
 def index(request):
@@ -6,7 +9,22 @@ def index(request):
 
 
 def contact(request):
-    return render(request, "contact.html")
+    form = ContactForm(request.POST or None)
+
+    if str(request.method) == "POST":
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            subject = form.cleaned_data["subject"]
+            message = form.cleaned_data["message"]
+
+            messages.success(request, "Email was send with success")
+            form = ContactForm()
+        else:
+            messages.error(request, "Error to send email")
+
+    context = {"form": form}
+    return render(request, "contact.html", context)
 
 
 def product(request):
